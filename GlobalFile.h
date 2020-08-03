39672732a41d7c5523e0fa0f0bfd69c31354b126
Ricky-lab <68934756+Ricky-lab@users.noreply.github.com>
2020-08-04 05:01:21 +0800

    Update GlobalFile.h

--- GlobalFile.h
+++ GlobalFile.h
@@ -3,5 +3,5 @@
 #define ADMIN_FILE      "admin.txt"
 #define STUDENT_FILE    "student.txt"
 #define TEACHER_FILE    "teacher.txt"
-#define COMPUTER_FILE   "computer.txt"
+#define COMPUTER_FILE   "computerRoom.txt"
 #define ORDER_FILE      "order.txt"

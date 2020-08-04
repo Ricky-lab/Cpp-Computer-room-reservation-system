8e16675600e5a57b8431bfb4e3c9b657424b731b
Ricky-lab <68934756+Ricky-lab@users.noreply.github.com>
2020-08-04 12:53:07 +0800

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,1 +1,36 @@
 #pragma once
+#include <iostream>
+using namespace std;
+#include "Identity.h"
+#include<vector>
+#include"ComputerRoom.h"
+#include<fstream>
+#include"GlobalFile.h"
+#include"orderFile.h"
+
+class Student :public Identity
+{
+public:
+	Student();
+
+	Student(int id, string name, string pwd);
+
+	virtual void operMenu() ;
+
+	void applyOrder();
+
+	void showMyOrder();
+
+	void showAllOrder();
+
+	void cancelOrder();
+
+	int m_Id;
+
+	//container of the Computer Room
+	vector<ComputerRoom> vCom;
+	
+private:
+
+};
+

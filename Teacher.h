badce980fb97b53b2db50bffeddc68c24ea291f0
Ricky-lab <68934756+Ricky-lab@users.noreply.github.com>
2020-08-04 12:53:34 +0800

    Update Teacher.h

--- Teacher.h
+++ Teacher.h
@@ -1,1 +1,27 @@
 #pragma once
+#include <iostream>
+using namespace std;
+#include"Identity.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include"orderFile.h"
+#include<vector>
+
+class Teacher :public Identity
+{
+public:
+	Teacher ();
+
+	Teacher(int empId, string name, string pwd);
+
+	virtual void operMenu() ;
+
+	void showAllOrder();
+
+	void valiOrder();
+
+	int m_EmpId;
+
+private:
+
+};
+
